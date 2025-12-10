7e05d01499ee36ebeeaaa75bbbb133f7cf08e11e
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-10 11:13:20 -0300

    add post and error routes

--- src/router.php
+++ src/router.php
@@ -5,13 +5,12 @@ $path = $urlComponents['path'];
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/post' => 'controllers/post.php',
+    '/error' => 'controllers/error.php',
 ];
-
 
 if (array_key_exists($path, $routes)) {
     require $routes[$path];
 } else {
-    http_response_code(404);
-    echo "404 Not Found";
-    die();
+    abort(404);
 }
