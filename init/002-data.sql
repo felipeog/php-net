2d25cc06781a205bac45e21224efb5371ef5594b
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-10 01:25:17 -0300

    add mysql to docker

---
+++ init/002-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    posts (title, content)
+VALUES
+    (
+        'First Post',
+        'This is the content of the first post.'
+    ),
+    (
+        'Second Post',
+        'This is the content of the second post.'
+    );
