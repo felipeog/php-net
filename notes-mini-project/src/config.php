1c375bd6502270939ba545e49ac07afab0b6466f
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-19 12:16:02 -0300

    add config

---
+++ notes-mini-project/src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dbDsn' => [
+        'host' => 'db',
+        'port' => '3306',
+        'dbname' => 'database',
+        'charset' => 'utf8mb4',
+    ],
+    'dbCredentials' => [
+        'username' => 'user',
+        'password' => 'userpass',
+    ],
+];
