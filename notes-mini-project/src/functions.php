2a33dfb692244410adade2339018ae02de35b8f2
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-19 12:16:09 -0300

    add functions

---
+++ notes-mini-project/src/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function isCurrentUri($uri)
+{
+    return $_SERVER['REQUEST_URI'] === $uri;
+}
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function abort($code = Response::NOT_FOUND)
+{
+    header("Location: /error?code={$code}");
+
+    die();
+}
+
+function authorize($condition, $code = Response::FORBIDDEN)
+{
+    if (!$condition) {
+        abort($code);
+    }
+}
