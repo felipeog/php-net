909721d74787c37c89f858e878ce9b993ca76af4
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-23 19:16:49 -0300

    add routes

--- sessions-and-authentication/src/routes.php
+++ sessions-and-authentication/src/routes.php
@@ -12,3 +12,6 @@ $router->get('/notes/create', 'controllers/notes/create.php');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/note/edit', 'controllers/notes/edit.php');
+
+$router->get('/register', 'controllers/registration/create.php');
+$router->post('/register', 'controllers/registration/store.php');
