ab80464ba0c6b3aba23e48d2ed0844ff76f28ca0
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-23 22:28:11 -0300

    create login and logout functions

--- sessions-and-authentication/src/Core/functions.php
+++ sessions-and-authentication/src/Core/functions.php
@@ -47,3 +47,30 @@ function partial($path, $attributes = [])
 
     require base_path("/views/partials/{$path}");
 }
+
+function login($user)
+{
+    $_SESSION['user'] = [
+        'email' => $user['email']
+    ];
+
+    session_regenerate_id(true);
+}
+
+function logout()
+{
+    $_SESSION = [];
+    $params = session_get_cookie_params();
+    $expires = time() - 3_600;
+
+    session_destroy();
+    setcookie(
+        'PHPSESSID',
+        '',
+        $expires,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
