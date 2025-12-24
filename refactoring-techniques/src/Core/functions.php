4dfc1916f34a1cc06495caccfdc6fb4b5f6e1d5e
felipeog <17603069+felipeog@users.noreply.github.com>
2025-12-24 18:53:43 -0300

    remove moved functions

--- refactoring-techniques/src/Core/functions.php
+++ refactoring-techniques/src/Core/functions.php
@@ -48,30 +48,8 @@ function partial($path, $attributes = [])
     require base_path("/views/partials/{$path}");
 }
 
-function login($user)
+function redirect($path)
 {
-    $_SESSION['user'] = [
-        'email' => $user['email'],
-        'id' => (int) $user['id']
-    ];
-
-    session_regenerate_id(true);
-}
-
-function logout()
-{
-    $_SESSION = [];
-    $params = session_get_cookie_params();
-    $expires = time() - 3_600;
-
-    session_destroy();
-    setcookie(
-        'PHPSESSID',
-        '',
-        $expires,
-        $params['path'],
-        $params['domain'],
-        $params['secure'],
-        $params['httponly']
-    );
+    header("Location: {$path}");
+    die();
 }
